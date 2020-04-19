27c20888930500609e87a413db7fea7032fae74c
Sunday <s5935512004@phuket.psu.ac.th>
2020-04-19 15:24:31 +0700

    :bulb: update movie

--- src/movies/movie.controller.ts
+++ src/movies/movie.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Post, Body } from '@nestjs/common'
+import { Controller, Post, Body, Get, Param, Patch } from '@nestjs/common'
 
 import { MoviesService } from './movie.service'
 
 @Controller('movies')
 export class MoviesController {
-    constructor(private readonly moviessService: MoviesService) {
+    constructor(private readonly moviesService: MoviesService) {
 
     }
     @Post()
@@ -14,7 +14,29 @@ export class MoviesController {
         @Body('distributor') prodDistributor: string,
         @Body('gross') prodGross: number,
     ) {
-        const generatedId = this.moviessService.insertMovie(prodTitle, prodRank, prodDistributor, prodGross)
+        const generatedId = this.moviesService.insertMovie(prodTitle, prodRank, prodDistributor, prodGross)
         return { id: generatedId }
+    }
+
+    @Get()
+    getAllMovies() {
+        return this.moviesService.getMovies()
+    }
+
+    @Get(':id')
+    getMovies(@Param('id') prodId: string, ) {
+        return this.moviesService.getSingleMovie(prodId)
+    }
+
+    @Patch(':id')
+    updateMovie(
+        @Param('id') prodId: string,
+        @Body('title') prodTitle: string,
+        @Body('rank') prodRank: number,
+        @Body('distributor') prodDistributor: string,
+        @Body('gross') prodGross: number,
+    ) {
+        this.moviesService.updateMovie(prodId, prodTitle, prodRank, prodDistributor, prodGross)
+        return null
     }
 }
--- src/movies/movie.service.ts
+++ src/movies/movie.service.ts
@@ -1,14 +1,50 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { Movie } from './movie.model'
 
 @Injectable()
 export class MoviesService {
-    movies: Movie[] = []
+    private movies: Movie[] = []
 
     insertMovie(title: string, rank: number, distributor: string, gross: number) {
-        const prodId = new Date().toString()
+        const prodId = Math.random().toString()
         const newMovies = new Movie(prodId, title, rank, distributor, gross)
         this.movies.push(newMovies)
         return prodId
+    }
+
+    getMovies() {
+        return [...this.movies]
+    }
+
+    getSingleMovie(prodId: string) {
+        const movie = this.findMovie(prodId)[0]
+        return { ...movie }
+    }
+
+    updateMovie(prodId: string, title: string, rank: number, distributor: string, gross: number) {
+        const [movie, index] = this.findMovie(prodId)
+        const updatedMovie = { ...movie }
+        if (title) {
+            updatedMovie.title = title
+        }
+        if (rank) {
+            updatedMovie.rank = rank
+        }
+        if (distributor) {
+            updatedMovie.distributor = distributor
+        }
+        if (gross) {
+            updatedMovie.gross = gross
+        }
+        this.movies[index] = updatedMovie
+    }
+
+    private findMovie(id: string): [Movie, number] {
+        const moviIndex = this.movies.findIndex((prod) => prod.id === id)
+        const movie = this.movies[moviIndex]
+        if (!movie) {
+            throw new NotFoundException('Could not find movie.')
+        }
+        return [movie, moviIndex]
     }
 }
